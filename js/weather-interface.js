aabb68ca4060f360561e76a35576390dfa0d7350
Musab36 <musabhd@gmail.com>
2017-06-29 18:15:49 +0300

    pingpong

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,14 +1,12 @@
+var Weather = require('./../js/weather.js').weatherModule;
+var apiKey = require('./../.env').apiKey;
 var apiKey = "b688b3d7d1b82f8152d871eda6266e16";
 
 $(document).ready(function() {
+  var currentWeatherObject = new weather();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showweather').text("The city you have chosen is " + city + ",");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=' + apiKey, function(response {
-      $('.showweather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-      $('.showweather').text(error.responseJSON.message);
+    currentWeatherObject.getWeather(city);
     });
   });
-});
